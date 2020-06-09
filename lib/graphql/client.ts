ac669a129c1531f6e6fcb40b63fd86f15327639f
Kazuhito Hokamura <k.hokamura@gmail.com>
2020-06-09 23:07:48 +0900

    Set API endpoint host for production

--- lib/graphql/client.ts
+++ lib/graphql/client.ts
@@ -1,7 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated";
-import { useEffect, useState } from "react";
 
-const host = "http://localhost:5200";
+const host =
+  process.env.NODE_ENV === "production"
+    ? "https://tolymer-one-api.herokuapp.com"
+    : "http://localhost:5200";
 
 export const graphqlClient = getSdk(new GraphQLClient(`${host}/graphql`));
